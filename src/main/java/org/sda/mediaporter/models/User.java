99def355b8c4928fab13ab6f7ac2204fef51704c
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-04-15 19:52:59 +0200

    install spring

---
+++ src/main/java/org/sda/mediaporter/models/User.java
@@ -0,0 +1,5 @@
+package org.sda.mediaporter.models;
+
+public class User {
+
+}
---
+++ src/test/java/org/sda/mediaporter/MediaPorterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sda.mediaporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaPorterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

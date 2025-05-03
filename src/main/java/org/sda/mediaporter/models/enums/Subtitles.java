0e4e02a6a5548054694e0b1ce7b14df9e5afac6f
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-05-03 20:41:37 +0200

    create subtitles enum

---
+++ src/main/java/org/sda/mediaporter/models/enums/Subtitles.java
@@ -0,0 +1,24 @@
+package org.sda.mediaporter.models.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Subtitles {
+    SUBRIP("subrip", ".srt"),
+    ASS("ass", ".ass"),
+    SSA("ssa", ".ssa"),
+    MOV_TEXT("mov_text", ".mp4"),
+    WEBVTT("webvtt", ".vtt"),
+    DVB_SUBTITLE("dvb_subtitle", ".sub"),
+    HDMV_PGS_SUBTITLE("hdmv_pgs_subtitle", ".sup"),
+    DVD_SUBTITLE("dvd_subtitle", ".sub"),
+    UNKNOWN("unknown", "");
+
+    private final String ffmpegName;
+    private final String extension;
+
+    Subtitles(String ffmpegName, String extension) {
+        this.ffmpegName = ffmpegName;
+        this.extension = extension;
+    }
+}

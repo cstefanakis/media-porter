417c6ed3afcbddd22b10bedf6779c428684ab515
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-08-24 21:40:39 +0200

    prepare tests

---
+++ src/test/java/org/sda/mediaporter/Services/Impl/ConfigurationServiceImplTest.java
@@ -0,0 +1,44 @@
+package org.sda.mediaporter.Services.Impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@ActiveProfiles("Test")
+class ConfigurationServiceImplTest {
+
+    @Test
+    void getConfiguration() {
+    }
+
+    @Test
+    void getGenresFromConfiguration() {
+    }
+
+    @Test
+    void getVideoResolutionFromConfiguration() {
+    }
+
+    @Test
+    void getVideoCodecsFromConfiguration() {
+    }
+
+    @Test
+    void getAudioCodecsFromConfiguration() {
+    }
+
+    @Test
+    void getAudioChannelsFromConfiguration() {
+    }
+
+    @Test
+    void getLanguagesFromConfiguration() {
+    }
+
+    @Test
+    void updateConfiguration() {
+    }
+}

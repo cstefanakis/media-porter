a53f87f18eb622b91a689255ba7ad3e796385252
I have an idea <49106207+christosste86@users.noreply.github.com>
2025-10-29 23:04:21 +0100

    create apiConnection for theMovieDbTvShow episode details find by id and test

--- src/main/java/org/sda/mediaporter/api/TheMovieDbTvShowEpisodes.java
+++ src/main/java/org/sda/mediaporter/api/TheMovieDbTvShowEpisodes.java
@@ -1,14 +1,136 @@
 package org.sda.mediaporter.api;
 
+import lombok.Getter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.sda.mediaporter.models.Contributor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TheMovieDbTvShowEpisodes {
 
-    private ApiConnect TheMovieDB;
     private final String apiKey = "7c97b163195d9428522398e8f1c32f63";
-    private int resultsObjectIndex;
+    private final JSONObject result;
 
+    @Getter
+    private LocalDate airDate;
+    @Getter
+    private List<Contributor> writers;
+    @Getter
+    private List<Contributor> directors;
+    @Getter
+    private List<Contributor> actors;
+    @Getter
+    private Integer episodeNumber;
+    @Getter
+    private String episodeType;
+    @Getter
+    private Integer seasonNumber;
+    @Getter
+    private String episodeName;
+    @Getter
+    private String overview;
+    @Getter
+    private Double rate;
+    @Getter
+    private String image;
 
-    public TheMovieDbTvShowEpisodes(String tvShowId, Integer season, Integer episode) {
-        String url = String.format("GET https://api.themoviedb.org/3/tv/%s/season/%s/episode/%s?api_key=%s&language=en-US",tvShowId, season, episode, apiKey);
-        this.TheMovieDB = new ApiConnect(url);
+    public TheMovieDbTvShowEpisodes(int tvShowId, int season, int episode) {
+        this.result = result(tvShowId, season, episode);
+        this.airDate = airDate();
+        this.writers = contributorsDirectorsAndWriters(this.result, "Writer");
+        this.directors = contributorsDirectorsAndWriters(this.result,"Director");
+        this.actors = contributorsActors(this.result);
+        this.episodeNumber = getValidatedIntegerObject(this.result, "episode_number");
+        this.episodeType = getValidatedStringObject(this.result, "episode_type");
+        this.seasonNumber = getValidatedIntegerObject(this.result, "season_number");
+        this.episodeName = getValidatedStringObject(this.result, "name");
+        this.overview = getValidatedStringObject(this.result, "overview");
+        this.rate = getValidatedRate();
+        this.image = String.format("https://image.tmdb.org/t/p/w500%s",getValidatedStringObject(this.result, "still_path"));
+    }
+
+    private JSONObject result(int tvShowId, int season, int episode){
+        String url = String.format("https://api.themoviedb.org/3/tv/%s/season/%s/episode/%s?api_key=%s&language=en-US",tvShowId, season, episode, apiKey);
+        ApiConnect apiConnect = new ApiConnect(url);
+        String jsonFile = apiConnect.getJsonString();
+        return new JSONObject(jsonFile);
+    }
+
+    private LocalDate airDate(){
+        String key = "air_date";
+        if (this.result.has(key)) {
+            String airDate =  this.result.getString(key);
+            return LocalDate.parse(airDate);
+        }return null;
+    }
+
+    private List<Contributor> contributorsDirectorsAndWriters(JSONObject object, String job){
+        List<Contributor> constributorsList = new ArrayList<>();
+
+        JSONArray contributors = new JSONArray();
+
+        String objectKey = "crew";
+        if(object.has(objectKey)) {
+            contributors = this.result.getJSONArray("crew");
+        }
+
+        for (int i = 0; i < contributors.length(); i++) {
+
+            JSONObject contributor = contributors.getJSONObject(i);
+
+            String jobKey = "job";
+
+            if(contributor.has(jobKey) && contributor.getString(jobKey).equals(job)){
+                constributorsList.add(Contributor.builder()
+                        .fullName(getValidatedStringObject(contributor, "original_name"))
+                        .poster(String.format("https://image.tmdb.org/t/p/w500%s",
+                                getValidatedStringObject(contributor, "profile_path")))
+                        .build());
+            }
+
+        } return constributorsList;
+    }
+
+    private List<Contributor> contributorsActors(JSONObject object){
+        List<Contributor> contributorsList = new ArrayList<>();
+
+        String key = "guest_stars";
+
+        if(object.has(key)){
+
+            JSONArray contributors = object.getJSONArray(key);
+
+            for (int i = 0; i < contributors.length(); i++) {
+
+                JSONObject actor = contributors.getJSONObject(i);
+
+                contributorsList.add(Contributor.builder()
+                                .fullName(getValidatedStringObject(actor, "original_name"))
+                                .poster(String.format("https://image.tmdb.org/t/p/w500%s",
+                                        getValidatedStringObject(actor, "profile_path")))
+                        .build());
+            }
+        }return contributorsList;
+    }
+
+    private Integer getValidatedIntegerObject(JSONObject object, String key) {
+        return object.has(key)
+                ? object.getInt(key)
+                : null;
+    }
+
+    private Double getValidatedRate() {
+        return this.result.has("vote_average")
+                ? this.result.getDouble("vote_average")
+                : null;
+    }
+
+    private String getValidatedStringObject(JSONObject object, String key){
+        return object.has(key) && !object.isNull(key)
+                ? object.getString(key)
+                : null;
     }
 }
---
+++ src/test/java/org/sda/mediaporter/api/TheMovieDbTvShowEpisodesTest.java
@@ -0,0 +1,181 @@
+package org.sda.mediaporter.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.sda.mediaporter.models.Contributor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TheMovieDbTvShowEpisodesTest {
+
+    private TheMovieDbTvShowEpisodes dexter;
+
+    @BeforeEach
+    void setup(){
+        this.dexter = new TheMovieDbTvShowEpisodes(259909, 1, 1);
+    }
+
+    @Test
+    void getAirDate() {
+        //Arrest
+        LocalDate airDate = LocalDate.of(2025, 7, 13);
+
+        //Act
+        LocalDate result = dexter.getAirDate();
+
+        //Assert
+        assertEquals(airDate, result);
+
+    }
+
+    @Test
+    void getWriters() {
+        //Arrest
+        String writer = "Clyde Phillips";
+        String poster = "https://image.tmdb.org/t/p/w500"+"/ue6gnd9FRbl5wKZ9yHVtWp6ra4y.jpg";
+
+        //Act
+        List<Contributor> result = dexter.getWriters();
+        //Assert
+        assertFalse(result.stream()
+                .filter(n -> n.getFullName().equals(writer))
+                .toList()
+                .isEmpty());
+
+        assertFalse(result.stream()
+                .filter(n -> n.getPoster().equals(poster))
+                .toList()
+                .isEmpty());
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void getDirectors() {
+        //Arrest
+        String director = "Marcos Siega";
+        String poster = "https://image.tmdb.org/t/p/w500"+"/uZD2ihL4iKzQxMtcLDH47AikT1V.jpg";
+
+        //Act
+        List<Contributor> result = dexter.getDirectors();
+        //Assert
+        assertFalse(result.stream()
+                .filter(n -> n.getFullName().equals(director))
+                .toList()
+                .isEmpty());
+
+        assertFalse(result.stream()
+                .filter(n -> n.getPoster().equals(poster))
+                .toList()
+                .isEmpty());
+
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void getActors() {
+        //Arrest
+        String actor = "John Lithgow";
+        String poster = "https://image.tmdb.org/t/p/w500"+"/8Y1sjBdnVR483S8PrnAQzlESwhx.jpg";
+
+        //Act
+        List<Contributor> result = dexter.getActors();
+        //Assert
+        assertFalse(result.stream()
+                .filter(n -> n.getFullName().equals(actor))
+                .toList()
+                .isEmpty());
+
+        assertFalse(result.stream()
+                .filter(n -> n.getPoster().equals(poster))
+                .toList()
+                .isEmpty());
+    }
+
+    @Test
+    void getEpisodeNumber() {
+        //Arrest
+        Integer episodeNumber = 1;
+
+        //Act
+        Integer result = dexter.getEpisodeNumber();
+
+        //Assert
+        assertEquals(episodeNumber, result);
+    }
+
+    @Test
+    void getEpisodeType() {
+        //Arrest
+        String episodeType = "standard";
+
+        //Act
+        String result = dexter.getEpisodeType();
+
+        //Assert
+        assertEquals(episodeType, result);
+    }
+
+    @Test
+    void getSeasonNumber() {
+        //Arrest
+        Integer seasonNumber = 1;
+
+        //Act
+        Integer result = dexter.getSeasonNumber();
+
+        //Assert
+        assertEquals(seasonNumber, result);
+    }
+
+    @Test
+    void getEpisodeName() {
+        //Arrest
+        String episodeName = "A Beating Heart...";
+
+        //Act
+        String result = dexter.getEpisodeName();
+
+        //Assert
+        assertEquals(episodeName, result);
+    }
+
+    @Test
+    void getOverview() {
+        //Arrest
+        String overview = "Dexter Morgan awakens from a ten-week coma.";
+
+        //Act
+        String result = dexter.getOverview();
+
+        //Assert
+        assertEquals(overview, result);
+    }
+
+    @Test
+    void getRate() {
+        //Arrest
+        Double overview = 6.7;
+
+        //Act
+        Double result = dexter.getRate();
+
+        //Assert
+        assertEquals(overview, result);
+    }
+
+    @Test
+    void getImage() {
+        //Arrest
+        String image = "https://image.tmdb.org/t/p/w500"+"/cdxZjP0S2K1FGO3NiXbQAM6TqwS.jpg";
+
+        //Act
+        String result = dexter.getImage();
+
+        //Assert
+        assertEquals(image, result);
+    }
+}

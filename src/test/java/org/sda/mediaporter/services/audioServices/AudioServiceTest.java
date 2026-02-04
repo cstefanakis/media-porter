ed16629b5d361541c39b9b166dbe9455b14bdfc0
chris <christosste86@gmail.com>
2026-02-04 21:51:52 +0100

    create AudioService tests

--- src/test/java/org/sda/mediaporter/services/audioServices/AudioServiceTest.java
+++ src/test/java/org/sda/mediaporter/services/audioServices/AudioServiceTest.java
@@ -6,13 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.sda.mediaporter.models.Language;
+import org.sda.mediaporter.dtos.AudioDto;
 import org.sda.mediaporter.models.VideoFilePath;
 import org.sda.mediaporter.models.enums.MediaTypes;
 import org.sda.mediaporter.models.metadata.Audio;
 import org.sda.mediaporter.models.metadata.AudioChannel;
 import org.sda.mediaporter.models.metadata.Codec;
-import org.sda.mediaporter.repositories.LanguageRepository;
 import org.sda.mediaporter.repositories.metadata.AudioRepository;
 import org.sda.mediaporter.services.CodecService;
 import org.sda.mediaporter.services.audioServices.impl.AudioServiceImpl;
@@ -22,7 +21,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AudioServiceTest {
@@ -36,17 +35,13 @@ class AudioServiceTest {
     @Mock
     private CodecService codecService;
 
-    @Mock
-    private LanguageRepository languageRepository;
-
     @InjectMocks
     private AudioServiceImpl audioService;
 
-    private Audio audio;
     private AudioChannel audioChannel;
-    private Codec videoCodec;
-    private Language language;
     private Codec audioCodec;
+    private final Path moviePath = Path.of("src/test/resources/files/movies/Thor (2011) - Official Trailer [HD] (816p_24fps_H264-128kbit_AAC).mp4").normalize();
+    private VideoFilePath videoFilePath;
 
     @BeforeEach
     void loadData(){
@@ -55,28 +50,13 @@ class AudioServiceTest {
                 .title("Stereo")
                 .build();
 
-        this.videoCodec = Codec.builder()
-                .name("H264")
-                .mediaType(MediaTypes.VIDEO)
-                .build();
-
         this.audioCodec = Codec.builder()
                 .name("AAC")
                 .mediaType(MediaTypes.VIDEO)
                 .build();
 
-        this.language = Language.builder()
-                .englishTitle("English")
-                .iso6391("En")
-                .iso6392B("Eng")
-                .iso6392T("Eng")
-                .build();
-
-        this.audio = Audio.builder()
-                .audioChannel(audioChannel)
-                .codec(audioCodec)
-                .language(language)
-                .bitrate(128)
+        this.videoFilePath = VideoFilePath.builder()
+                .filePath(this.moviePath.toString())
                 .build();
     }
 
@@ -88,26 +68,56 @@ class AudioServiceTest {
         when(codecService.getOrCreateCodecByCodecNameAndMediaType(audioCodec, MediaTypes.AUDIO)).thenReturn(this.audioCodec);
         when(audioRepository.save(any(Audio.class)))
                 .thenAnswer(invocation -> invocation.getArgument(0));
-        Path path = Path.of("src/test/resources/files/movies/Thor (2011) - Official Trailer [HD] (816p_24fps_H264-128kbit_AAC).mp4").normalize();
-        VideoFilePath videoFilePath = new VideoFilePath();
         //Act
-        List<Audio> result = audioService.getCreatedAudiosFromPathFile(path, videoFilePath);
+        List<Audio> result = audioService.getCreatedAudiosFromPathFile(this.moviePath, this.videoFilePath);
         //Assert
         assertNotNull(result);
         assertTrue(result.stream().anyMatch(a -> a.getAudioChannel().getChannels() == 2));
         assertTrue(result.stream().anyMatch(a -> a.getCodec().getName().equalsIgnoreCase(audioCodec)));
+        verify(audioChannelService, times(1)).getAudioChannelByChannelsOrNull(2);
+        verify(codecService, times(1)).getOrCreateCodecByCodecNameAndMediaType(audioCodec, MediaTypes.AUDIO);
     }
 
     @Test
     void getAudiosFromPathFile() {
-
+        //Arrest
+        String audioCodec = "Aac";
+        when(audioChannelService.getAudioChannelByChannelsOrNull(2)).thenReturn(this.audioChannel);
+        when(codecService.getOrCreateCodecByCodecNameAndMediaType(audioCodec, MediaTypes.AUDIO)).thenReturn(this.audioCodec);
+        //Act
+        List<Audio> result = audioService.getAudiosFromPathFile(this.moviePath, this.videoFilePath);
+        //Assert
+        assertNotNull(result);
+        assertTrue(result.stream().anyMatch(a -> a.getAudioChannel().getChannels() == 2));
+        assertTrue(result.stream().anyMatch(a -> a.getCodec().getName().equalsIgnoreCase(audioCodec)));
+        verify(audioChannelService, times(1)).getAudioChannelByChannelsOrNull(2);
+        verify(codecService, times(1)).getOrCreateCodecByCodecNameAndMediaType(audioCodec, MediaTypes.AUDIO);
     }
 
     @Test
     void deleteAudioById() {
+        //Arrest
+        Long audioId = 1L;
+        doNothing().when(audioRepository).deleteById(audioId);
+        //Act
+        audioService.deleteAudioById(audioId);
+        //Assert
+        verify(audioRepository, times(1)).deleteById(audioId);
     }
 
     @Test
     void getAudiosDetails() {
+        //Arrest
+        String codec = "aac";
+        Integer channels = 2;
+        Integer bitrate= null;
+        //Act
+        List<AudioDto> result = audioService.getAudiosDetails(this.moviePath);
+        //Assert
+        assertNotNull(result);
+        assertTrue(result.stream().anyMatch(dto -> dto.getAudioCodec().equalsIgnoreCase(codec)));
+        assertTrue(result.stream().anyMatch(dto -> dto.getAudioBitrate() == null));
+        assertTrue(result.stream().anyMatch(dto -> dto.getAudioChannel().equals(channels)));
+        assertTrue(result.stream().anyMatch(dto -> dto.getAudioLanguage() == null));
     }
 }
